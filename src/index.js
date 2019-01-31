8aacd3f9928f10f438c21997674b760b778bf046
Leslie Shea <leslieyshea@gmail.com>
2019-01-31 11:59:33 -0800

    gave button elements text, value, and class and appended the child node to parent node

--- src/index.js
+++ src/index.js
@@ -10,15 +10,25 @@ const colors = [
     'yellow'
 ];
 
+// create button elements
 // goal is to loop through array, and assign textContent to the buttons
-// console.log after declaring color variable to ensure it pulls the array
+// add text (textContent), class (class ids), and value to the button
+// append button to parent node
 for(let index = 0; index < colors.length; index++) {
     let color = colors[index];
-    const colorButtonNode = document.createElement('button');
-    console.log(colorButtonNode);
+    const colorButtonNode = document.createElement('button'); // creates a new button during each loop
+    colorButtonNode.textContent = color; // gives each button text
+    colorButtonNode.value = color; // gives each button value
+    colorButtonNode.classList.add('color-button'); // gives each button a class id
+    
+    // call back function; won't fire until button is clicked
+    colorButtonNode.addEventListener('click', function() {
+        console.log(colorButtonNode.value + ' clicked!');
+    });
+
+    colorButtonsNode.appendChild(colorButtonNode); // appends this child node to the parent colorButtonsNode
 }
 
 
 
 
-// create button elements
